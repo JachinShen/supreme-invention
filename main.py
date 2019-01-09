b889280f9c1f61cd7cabb2d083dd85378d2b2157
JachinShen <jachinshen@foxmail.com>
2019-01-09 18:57:16 +0800

    init

---
+++ main.py
@@ -0,0 +1,232 @@
+import sys, math
+import numpy as np
+
+import Box2D
+from Box2D.b2 import (edgeShape, circleShape, fixtureDef, polygonShape, revoluteJointDef, contactListener)
+
+import gym
+from gym import spaces
+#from gym.envs.box2d.car_dynamics import Car
+from gym.utils import colorize, seeding, EzPickle
+
+import pyglet
+from pyglet import gl
+
+from Robot import Car
+from ICRAMap import ICRAMap
+
+STATE_W = 96   # less than Atari 160x192
+STATE_H = 96
+VIDEO_W = 600
+VIDEO_H = 400
+WINDOW_W = 1200
+WINDOW_H = 1000
+
+SCALE       = 4.0        # Track scale
+TRACK_RAD   = 900/SCALE  # Track is heavily morphed circle with this radius
+PLAYFIELD   = 2000/SCALE # Game over boundary
+FPS         = 50
+ZOOM        = 2.7        # Camera zoom
+ZOOM_FOLLOW = True       # Set to False for fixed view (don't use zoom)
+
+class CarRacing(gym.Env, EzPickle):
+    metadata = {
+        'render.modes': ['human', 'rgb_array', 'state_pixels'],
+        'video.frames_per_second' : FPS
+    }
+
+    def __init__(self):
+        EzPickle.__init__(self)
+        self.seed()
+        #self.contactListener_keepref = FrictionDetector(self)
+        #self.world = Box2D.b2World((0,0), contactListener=self.contactListener_keepref)
+        self.world = Box2D.b2World((0,0))
+        self.viewer = None
+        self.invisible_state_window = None
+        self.invisible_video_window = None
+        self.road = None
+        self.car = None
+        self.map = None
+        self.reward = 0.0
+        self.prev_reward = 0.0
+
+        self.action_space = spaces.Box( np.array([-1,0,0]), np.array([+1,+1,+1]), dtype=np.float32)  # steer, gas, brake
+        self.observation_space = spaces.Box(low=0, high=255, shape=(STATE_H, STATE_W, 3), dtype=np.uint8)
+
+
+    def seed(self, seed=None):
+        self.np_random, seed = seeding.np_random(seed)
+        return [seed]
+
+    def _destroy(self):
+        if self.car:
+            self.car.destroy()
+        if self.map:
+            self.map.destroy()
+
+    def reset(self):
+        self._destroy()
+        self.reward = 0.0
+        self.prev_reward = 0.0
+        self.t = 0.0
+        self.human_render = False
+
+        self.car = Car(self.world, 0.0, 0.0, 5.0)
+        self.map = ICRAMap(self.world)
+
+        return self.step(None)[0]
+
+    def step(self, action):
+        if action is not None:
+            self.car.steer(-action[0])
+            self.car.gas(action[1])
+            self.car.brake(action[2])
+
+        self.car.step(1.0/FPS)
+        self.world.Step(1.0/FPS, 6*30, 2*30)
+        self.t += 1.0/FPS
+
+        self.state = self.render("state_pixels")
+
+        step_reward = 0
+        done = False
+        if action is not None: # First step without action, called from reset()
+            self.reward -= 0.1
+            # We actually don't want to count fuel spent, we want car to be faster.
+            #self.reward -=  10 * self.car.fuel_spent / ENGINE_POWER
+            self.car.fuel_spent = 0.0
+            step_reward = self.reward - self.prev_reward
+            self.prev_reward = self.reward
+            x, y = self.car.hull.position
+            if abs(x) > PLAYFIELD or abs(y) > PLAYFIELD:
+                done = True
+                step_reward = -100
+
+        return self.state, step_reward, done, {}
+
+    def render(self, mode='human'):
+        if self.viewer is None:
+            from gym.envs.classic_control import rendering
+            self.viewer = rendering.Viewer(WINDOW_W, WINDOW_H)
+            self.score_label = pyglet.text.Label('0000', font_size=36,
+                x=20, y=WINDOW_H*2.5/40.00, anchor_x='left', anchor_y='center',
+                color=(255,255,255,255))
+            self.transform = rendering.Transform()
+
+        if "t" not in self.__dict__: return  # reset() not called yet
+
+        zoom = ZOOM*SCALE
+        zoom_state  = ZOOM*SCALE*STATE_W/WINDOW_W
+        zoom_video  = ZOOM*SCALE*VIDEO_W/WINDOW_W
+        #scroll_x = self.car.hull.position[0]
+        #scroll_y = self.car.hull.position[1]
+        #angle = -self.car.hull.angle
+        scroll_x = 0.0
+        scroll_y = 0.0
+        angle = 0
+        #vel = self.car.hull.linearVelocity
+        #if np.linalg.norm(vel) > 0.5:
+            #angle = math.atan2(vel[0], vel[1])
+        self.transform.set_scale(zoom, zoom)
+        self.transform.set_translation(
+            WINDOW_W/2 - (scroll_x*zoom*math.cos(angle) - scroll_y*zoom*math.sin(angle)),
+            WINDOW_H/2 - (scroll_x*zoom*math.sin(angle) + scroll_y*zoom*math.cos(angle)) )
+        #self.transform.set_rotation(angle)
+
+        self.car.draw(self.viewer, mode!="state_pixels")
+        self.map.draw(self.viewer)
+
+        arr = None
+        win = self.viewer.window
+        if mode != 'state_pixels':
+            win.switch_to()
+            win.dispatch_events()
+        if mode=="rgb_array" or mode=="state_pixels":
+            win.clear()
+            t = self.transform
+            if mode=='rgb_array':
+                VP_W = VIDEO_W
+                VP_H = VIDEO_H
+            else:
+                VP_W = STATE_W
+                VP_H = STATE_H
+            gl.glViewport(0, 0, VP_W, VP_H)
+            t.enable()
+            for geom in self.viewer.onetime_geoms:
+                geom.render()
+            t.disable()
+            self.render_indicators(WINDOW_W, WINDOW_H)  # TODO: find why 2x needed, wtf
+            image_data = pyglet.image.get_buffer_manager().get_color_buffer().get_image_data()
+            arr = np.fromstring(image_data.data, dtype=np.uint8, sep='')
+            arr = arr.reshape(VP_H, VP_W, 4)
+            arr = arr[::-1, :, 0:3]
+
+        if mode=="rgb_array" and not self.human_render: # agent can call or not call env.render() itself when recording video.
+            win.flip()
+
+        if mode=='human':
+            self.human_render = True
+            win.clear()
+            t = self.transform
+            gl.glViewport(0, 0, WINDOW_W, WINDOW_H)
+            t.enable()
+            for geom in self.viewer.onetime_geoms:
+                geom.render()
+            t.disable()
+            self.render_indicators(WINDOW_W, WINDOW_H)
+            win.flip()
+
+        self.viewer.onetime_geoms = []
+        return arr
+
+    def close(self):
+        if self.viewer is not None:
+            self.viewer.close()
+            self.viewer = None
+
+    def render_indicators(self, W, H):
+        self.score_label.text = "%04i" % self.reward
+        self.score_label.draw()
+
+
+if __name__=="__main__":
+    from pyglet.window import key
+    a = np.array( [0.0, 0.0, 0.0] )
+    def key_press(k, mod):
+        global restart
+        if k==0xff0d: restart = True
+        if k==key.LEFT:  a[0] = -1.0
+        if k==key.RIGHT: a[0] = +1.0
+        if k==key.UP:    a[1] = +1.0
+        if k==key.DOWN:  a[2] = +0.8   # set 1.0 for wheels to block to zero rotation
+    def key_release(k, mod):
+        if k==key.LEFT  and a[0]==-1.0: a[0] = 0
+        if k==key.RIGHT and a[0]==+1.0: a[0] = 0
+        if k==key.UP:    a[1] = 0
+        if k==key.DOWN:  a[2] = 0
+    env = CarRacing()
+    env.render()
+    record_video = False
+    if record_video:
+        env.monitor.start('/tmp/video-test', force=True)
+    env.viewer.window.on_key_press = key_press
+    env.viewer.window.on_key_release = key_release
+    while True:
+        env.reset()
+        total_reward = 0.0
+        steps = 0
+        restart = False
+        while True:
+            s, r, done, info = env.step(a)
+            total_reward += r
+            if steps % 200 == 0 or done:
+                print("\naction " + str(["{:+0.2f}".format(x) for x in a]))
+                print("step {} total_reward {:+0.2f}".format(steps, total_reward))
+                #import matplotlib.pyplot as plt
+                #plt.imshow(s)
+                #plt.savefig("test.jpeg")
+            steps += 1
+            if not record_video: # Faster, but you can as well call env.render() every time to play full window.
+                env.render()
+            if done or restart: break
+    env.close()
